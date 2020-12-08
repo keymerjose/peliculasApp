1076852c1bba47cee6cfd7d3ea4c50c05181d846
KeymerJ <keymerj@gmail.com>
2020-12-08 15:30:26 -0500

    Creacion del proyecto de prueba de peliculasApp

---
+++ src/app/services/movies.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { RespuestaMDB } from '../interfaces/interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+
+  constructor(private http: HttpClient) { }
+
+  getFeature(){
+    return this.http.get<RespuestaMDB>('https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22&api_key=dfed94104a22290852948030c0468f10&language=es');
+  }
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MoviesService } from '../services/movies.service';
+import { RespuestaMDB, Pelicula } from '../interfaces/interfaces';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
+  peliculasRecientes: Pelicula[] = [];
+  constructor(private MoviesService: MoviesService) {}
 
-  constructor() {}
-
+    ngOnInit(){
+      this.MoviesService.getFeature().subscribe(
+        resp => {
+          console.log('Resp', resp);
+          this.peliculasRecientes = resp.results;
+        }
+      );
+    }
 }
